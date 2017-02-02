b671e02fba5273179ba17ba6f4bcba41a9ded419
janco25 <rodolfo_puma@outlook.com>
2017-02-02 11:52:57 -0300

    resto del id

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,10 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	var n,n2;
+	n = parseInt(document.getElementById('n1').value);
+	n2 = parseInt(document.getElementById('n2').value);
+	alert(n % n2);
 	
+	alert("rsultados dos : "+ n2%n);
 }
