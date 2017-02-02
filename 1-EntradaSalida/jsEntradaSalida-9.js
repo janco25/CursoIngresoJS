8a71d11f5b1bd1b227628e1a86769b67e59716e2
janco25 <rodolfo_puma@outlook.com>
2017-02-02 12:14:01 -0300

    procentaje

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,9 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
+	var n,re;
+	n= parseInt(document.getElementById('sueldo').value);
+		re= n*10/100;
+		document.getElementById('resultado').value=re;
 	
 }
