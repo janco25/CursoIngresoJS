d15c3bb6c721e460b8cc4a1af0f3727550487ee5
janco25 <rodolfo_puma@outlook.com>
2017-02-08 11:31:20 -0300

    re5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var ed;
+ed = parseInt(document.getElementById('edad').value);
+if (ed>=17)
+    {
+     alert("no es adolecente");   
+    }
+    else{
+        alert("es menor de edad");
+    }
 
 }//FIN DE LA FUNCIÓN
