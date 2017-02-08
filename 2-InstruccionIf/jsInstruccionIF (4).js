0a751a69be05b0c9058f5ae2322a6547172b6fef
janco25 <rodolfo_puma@outlook.com>
2017-02-08 11:30:49 -0300

    re4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 
-
+var ed;
+ed = parseInt(document.getElementById('edad').value);
+if (ed>=13 && ed<=17){
+    var s= prompt("ingrese algo");
+	document.getElementById('resul').innerHTML="<h1>"+s+"</h1>";
+}else{
+	document.getElementById('resul').innerHTML = "<h1>menor o mayor</h1>";
+}
 
 }//FIN DE LA FUNCIÓN
