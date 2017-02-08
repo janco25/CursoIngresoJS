ff73468c34cf718ada55a8895d5bad72d81db11a
janco25 <rodolfo_puma@outlook.com>
2017-02-08 11:26:33 -0300

    resultatdo 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var ed;
+ed = parseInt(document.getElementById('edad').value);
+if (ed==15){
+	document.getElementById('resul').value=("niña bonita");
+}else{
+	alert("no hay");
+}
 
 
 }//FIN DE LA FUNCIÓN
