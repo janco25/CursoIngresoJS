2476303506614ead5b218385da5f443d6590e398
janco25 <rodolfo_puma@outlook.com>
2017-02-08 11:31:46 -0300

    re6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+var ed;
+ed = parseInt(document.getElementById('edad').value);
+if (ed>=18)
+    {
+     alert("es mayor de edad");   
+    }
+    else if (ed>=13 && ed<=17){
+        alert("es adolecente");
+    }else if ( ed >=1 && ed<13){
+        alert("es un niño");
+    }else 
+        {
+            alert("es un bebe");
+        }
 
 
 
