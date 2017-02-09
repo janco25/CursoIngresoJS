3c2f3adc5e81f353502ba92b998869d5fc9fcd38
janco25 <rodolfo_puma@outlook.com>
2017-02-09 12:13:09 -0300

    hora

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,10 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+var laHora = parseInt(document.getElementById('hora').value) ;
 
 //alert (laHora);
-	
+	switch(laHora)
+	{
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("es de mañana");
+			break;
+		default :
+			alert("es otro tiempo");
+			break;	
+	}
 	
 
 
