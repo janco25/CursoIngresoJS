df610ecc11f196801576e378c004839ae7e649da
janco25 <rodolfo_puma@outlook.com>
2017-02-09 12:07:43 -0300

    dos

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,10 +1,26 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
-
+var mesa = document.getElementById('mes').value;
+switch(mesa)
+{
+	
+	
+	case "Marzo":
+	case "Abril":
+	case "Junio":
+		alert("falta poco para el invierno");
+	break;
+	case "Julio":
+	case "Agosto":
+		alert("abrigate que hace frio!!!!!!!");
+	break;
+	
+	default:
+	alert("ya paso el frio hace calor !!!!!!!!");
+	break;
+}
 
 
 
