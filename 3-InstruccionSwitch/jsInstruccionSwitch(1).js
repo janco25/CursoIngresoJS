5beadda6b39f82bc13402226dd9bb4c53a42d41b
janco25 <rodolfo_puma@outlook.com>
2017-02-09 10:06:19 -0300

    meses

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,8 +1,25 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
+var mesa = document.getElementById('mes').value;
+switch(mesa)
+{
+	case "Enero":
+	alert("que comienze bien el año");
+	break;
+	case "Marzo":
+	alert("a clases");
+	break;
+	case "Julio":
+	alert("se vienen las vacaciones");
+	break;
+	case "Diciembre":
+	alert("felices vacaciones");
+	break;
+	default:
+	alert("en clases");
+	break;
+}
 
 
 
